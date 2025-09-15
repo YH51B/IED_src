a0b79831ca27f73e66866705d84b45d2f474d9e8
YH <yhun0913@gmail.com>
2025-09-15 15:11:50 +0900

    Add files via upload

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,23 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  while(!Serial){
+    ;
+  }
+}
+
+void loop() {
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+  for(int i=0; i<=5; i=i+1){
+  digitalWrite(PIN_LED, 1);
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  }
+  digitalWrite(PIN_LED, 1);
+  while(1){}
+}
